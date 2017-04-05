604a1ac601453e84ca69cdceca96b9a38dfeb2f7
Samuel Udoh <sudoh@spartaglobal.co>
2017-04-05 12:14:43 +0100

    production schript added to vagrant file

--- production.sh
+++ production.sh
@@ -2,23 +2,10 @@
 
 sudo rm -rf /etc/nginx/sites-available/default
 
-sudo touch /etc/nginx/sites-available/default
+sudo cp nginx.default /etc/nginx/sites-available/default
 
-# echo "server {
-#     listen 80;
-#
-#     server_name _;
-#
-#     location / {
-#         proxy_pass http://localhost:3000;
-#         proxy_http_version 1.1;
-#         proxy_set_header Upgrade $http_upgrade;
-#         proxy_set_header Connection 'upgrade';
-#         proxy_set_header Host $host;
-#         proxy_cache_bypass $http_upgrade;
-#     }
-# }" > /etc/nginx/sites-available/default
+#c/nginx/sites-available/default
 
-cat > /etc/nginx/sites-available/default << nginx.default
+# cat > /etc/nginx/sites-available/default << nginx.default
 
 sudo service nginx restart
