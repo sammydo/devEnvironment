16205256b565a50bce95bfb5f812e29676a8cdad
Samuel Udoh <sudoh@spartaglobal.co>
2017-04-06 11:03:38 +0100

    changed provison.sh back removed eval line

--- provision.sh
+++ provision.sh
@@ -9,6 +9,3 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 sudo npm install pm2 -g
-
-echo "eval `ssh-agent -s`" >> ~/.bashrc
-echo "ssh-add ~/.ssh/github" >> ~/.bashrc
