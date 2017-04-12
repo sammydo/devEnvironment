dd659089f82a48dff48414f68f9873c7fc376c19
Samuel Udoh <sudoh@spartaglobal.co>
2017-04-12 11:15:59 +0100

    deply.sh changed, added dev env pull && update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #pull down new code
 
+cd ~/devEnvironment
+git pull
+berks vendor cookbooks
+sudo chef-client --local-mode --runlist 'recipe[node-server]'
+
+
 cd ~/app
 git pull
 sudo npm install
